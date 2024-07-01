835a96b5297786afe8da3af8a51b8e28cd2a782f
abbaszade <muradabbaszade6@gmail.com>
2024-07-01 22:57:44 +0400

    update reservation controller

--- src/main/java/az/m10/dto/ReservationAdminResponse.java
+++ src/main/java/az/m10/dto/ReservationAdminResponse.java
@@ -1,6 +1,7 @@
 package az.m10.dto;
 
 import az.m10.domain.Reservation;
+import az.m10.domain.enums.ReservationStatus;
 import lombok.*;
 
 @Setter
@@ -19,11 +20,14 @@ public class ReservationAdminResponse {
 
     private Long volunteerId;
 
+    private String status;
+
     public ReservationAdminResponse(Reservation reservation) {
         this.reservationId = reservation.getId();
         this.locationId = reservation.getLocation().getId();
         this.startTime = reservation.getStartTime().toString();
         this.endTime = reservation.getEndTime().toString();
         this.volunteerId = reservation.getVolunteer().getId();
+        this.status = reservation.getStatus().toString();
     }
 }
