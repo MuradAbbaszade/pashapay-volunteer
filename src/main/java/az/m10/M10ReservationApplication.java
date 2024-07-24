3192bb95cd709d6793d6bbd670c7978b2dc80248
abbaszade <muradabbaszade6@gmail.com>
2024-07-24 15:11:36 +0400

    update command line runner

--- src/main/java/az/m10/M10ReservationApplication.java
+++ src/main/java/az/m10/M10ReservationApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootApplication
@@ -14,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class M10ReservationApplication implements CommandLineRunner {
 
     private final JdbcTemplate jdbcTemplate;
+    private final PasswordEncoder passwordEncoder;
 
     public static void main(String[] args) {
         SpringApplication.run(M10ReservationApplication.class, args);
@@ -23,7 +25,8 @@ public class M10ReservationApplication implements CommandLineRunner {
     @Transactional
     public void run(String... args) throws Exception {
         jdbcTemplate.update("INSERT INTO authorities(authority) VALUES('ADMIN')");
-        jdbcTemplate.update("INSERT INTO users(username, password, is_enabled) VALUES('admin','YDw04#0UE8<c',1)");
+        String encodedPassword = passwordEncoder.encode("YDw04#0UE8<c");
+        jdbcTemplate.update("INSERT INTO users(username, password, is_enabled) VALUES('admin',?,1)",encodedPassword);
         jdbcTemplate.update("INSERT INTO user_authorities(user_id, authority_id) VALUES(1,1)");
     }
 
